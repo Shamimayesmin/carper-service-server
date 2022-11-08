2d52d6df31ae8e771b146c41efd875609212119b
shamima <shamimayesmin144@gamil.com>
2022-11-08 08:35:39 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+// const jwt = require('jsonwebtoken')
+
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors())
+app.use(express.json());
+
+
+// user : carperDbUser
+// pass : shvWcrAwZ2og7MWY
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ui8slz3.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', (req,res) =>{
+    res.send('server is running')
+})
+
+app.listen(port, () =>{
+    console.log(`wood service server is running on ${port}`);
+})
