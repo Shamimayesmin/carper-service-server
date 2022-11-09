0c825cdbc9ad4ce12a19e205876899be42cdbd65
shamima <shamimayesmin144@gamil.com>
2022-11-09 13:24:54 +0600

    create review api

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectID } = require('bson');
+const { ObjectID, ObjectId } = require('bson');
+const { query } = require('express');
 require('dotenv').config()
 // const jwt = require('jsonwebtoken')
 
@@ -31,6 +32,8 @@ async function run(){
     try{
         const serviceCollection = client.db('carperDb').collection('services')
 
+        const reviewCollection = client.db('carperDb').collection('reviews')
+
 
         app.get('/services', async(req,res) =>{
             const query = {}
@@ -43,6 +46,53 @@ async function run(){
             const query = {_id : ObjectID(id)}
             const service = await serviceCollection.findOne(query)
             res.send(service)
+        })
+
+        // review api 
+        app.post('/reviews', async(req, res) =>{
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+
+        // get review api from db 
+        app.get('/reviews', async(req,res) =>{
+
+            // const decoded = req.decoded;
+            // if(decoded.email !== req.query.email){
+            //     res.status(403).send({message : 'unathorized access'})
+            // }
+            let query = {}
+            if(req.query.email){
+                query = {
+                    email : req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query) 
+            const review = await cursor.toArray()
+            res.send(review.reverse())
+        })
+
+        // update review page :
+        app.patch('/reviews/:id', async(req,res) =>{
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = {_id : ObjectId(id)}
+            const updatedDoc = {
+                $set:{
+                    status : status
+                }
+            }
+            const result = await reviewCollection.updateOne(query,updatedDoc)
+            res.send(result)
+        })
+
+        // delete review 
+        app.delete('/reviews/:id' , async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query)
+            res.send(result);
         })
     }
     finally{
